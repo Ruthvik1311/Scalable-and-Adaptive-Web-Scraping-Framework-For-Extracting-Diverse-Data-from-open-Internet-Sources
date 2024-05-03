3ced60b73a2130e11e41f4d50bbf189e417e4f57
Ruthvik <108657009+Ruthvik1311@users.noreply.github.com>
2024-05-04 00:19:43 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import cv2
+import torch
+import matplotlib.pyplot as plt
+import requests
+from tensorflow import keras
+from PIL import Image
+import dlib
+from flask import Flask
+import streamlit as st
+import pickle
+import joblib
+from sklearn import datasets
+from skimage import data
+from scipy import stats
+import roboflow
+import nltk
+import folium
+import yfinance as yf
+import imageio
+import imutils
+import pygame
+from django import forms
+# from pycryptodome import Random
+# from pycryptodome.Cipher import AES
+from cryptography.fernet import Fernet
+from geopy.geocoders import Nominatim
+import streamlit_folium
+from shapely.geometry import Point
+import transformers
+import pydeck as pdk
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('punkt')
+print('installtion done successfully')
+
+# Ill guide you how to perform module installtion on windows laptop
+# First of all please go through the Special installtion video that is there in the folder
+# it tells you what to do and what not to do, installation of python and all
+# then once you have done and followed that video come here and open on more cmd
+# when you do that it will download some modules depending on the speed of internet it will install the modules
+# once the module isntallation is done 
+# you will see the cursor blinking again, if you get and error try using chatgpt first, if still you cannot install them
+# call the dev team for support regarding basic installion, but only after you have tried the above step and options
+# once the installion is done open the cmd again
+# once that message pops up it means its done
+# thankyou for following this installation.
